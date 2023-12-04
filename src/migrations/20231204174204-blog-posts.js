bf5d0700f0ab41e5dc4f07462e8f2c6757c67590
Felipe Duarte <feduarte.dev@gmail.com>
2023-12-04 14:57:44 -0300

    requisito 1 pronto

---
+++ src/migrations/20231204174204-blog-posts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      published: {
+        type: Sequelize.DATE
+      },
+      updated: {
+        type: Sequelize.DATE
+      },
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
